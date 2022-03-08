8d06f026f13c386c89efe2fa8fc6867916320d5a
yvanmig <yvan.gimard@gmail.com>
2022-03-08 10:42:12 +0100

    create stats and trails template and style

--- controllers/StatistiqueController.php
+++ controllers/StatistiqueController.php
@@ -6,7 +6,15 @@ class StatistiqueController extends Controller
 {
     public function index(): void
     {
+
+        $statistics_test = [
+          ['Nombre de parcours créés', 5],
+          ['Note moyenne de vos parcours', 0.1],
+          ['Temps de jeu total sur vos parcours', '39:25'],
+          ['Note moyenne de vos parcours', 0.1],
+        ];
+
         $endpoint = $this->endpoint;
-        echo $this->twig->render('pages/statistics.html.twig', ['page' => $endpoint]);
+        echo $this->twig->render('pages/statistics.html.twig', ['page' => $endpoint, 'statistics' => $statistics_test]);
     }
 }
