d4ae6bc63b0f54e7b0d1927738b8a1c1acce605e
a.francisco <afarthuraf@gmail.com>
2022-03-09 11:36:39 +0100

    gestion des assets

--- framework/Controller.php
+++ framework/Controller.php
@@ -9,6 +9,7 @@ abstract class Controller
     protected \Twig\Environment $twig;
     protected array $config;
     protected string $endpoint;
+    protected string $asset;
 
     public function __construct()
     {
@@ -17,6 +18,13 @@ abstract class Controller
         $this->twig = new \Twig\Environment($loader);
 
         $this->config = (new Config())->config;
+
+        $this->asset = 'http://' . $_SERVER['HTTP_HOST'] . '/assets';
+
+        $assets = new \Twig\TwigFunction('assets', function (String $url) {
+            return $this->asset . DIRECTORY_SEPARATOR . $url;
+        });
+        $this->twig->addFunction($assets);
 
         $this->endpoint = strtolower(str_replace('Controller', '', get_class($this)));
     }
