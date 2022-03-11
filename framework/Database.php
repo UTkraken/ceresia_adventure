086ce6fc006a39672b5f64bdc02b3fc0eb36def3
a.francisco <afarthuraf@gmail.com>
2022-03-11 11:00:55 +0100

    fix database

--- framework/Database.php
+++ framework/Database.php
@@ -8,14 +8,13 @@ use PDOStatement;
 
 class Database extends PDO
 {
-    private ?PDO $PDOInstance;
 
     private static ?Database $instance = null;
 
     private function __construct()
     {
         $config = (new Config())->config;
-        $this->PDOInstance = new PDO($config['DATABASE_DRIVER'] . ':dbname='.$config['DATABASE_DBNAME'].';host='.$config['DATABASE_HOST'].';port='.$config['DATABASE_PORT'] . ";charset=utf8mb4",$config['DATABASE_USER'] ,$config['DATABASE_PASSWORD']);
+        parent::__construct($config['DATABASE_DRIVER'] . ':dbname='.$config['DATABASE_DBNAME'].';host='.$config['DATABASE_HOST'].';port='.$config['DATABASE_PORT'] . ";charset=utf8mb4",$config['DATABASE_USER'] ,$config['DATABASE_PASSWORD']);
     }
 
     public static function getInstance(): ?Database
@@ -25,17 +24,5 @@ class Database extends PDO
             self::$instance = new Database();
         }
         return self::$instance;
-    }
-
-    /**
-     * Exécute une requête SQL avec PDO
-     * @param $statement
-     * @param int $mode
-     * @param mixed ...$fetch_mode_args
-     * @return PDOStatement Retourne l'objet PDOStatement
-     */
-    public function query($statement, $mode = PDO::FETCH_ASSOC, ...$fetch_mode_args): PDOStatement
-    {
-        return $this->PDOInstance->query($statement, $mode, ...$fetch_mode_args);
     }
 }
