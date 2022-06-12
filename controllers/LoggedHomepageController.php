4810cec2341eb41376b3e8333ce670c6754b0681
yvanmig <yvan.gimard@gmail.com>
2022-06-12 16:54:46 +0200

    add method to only display trails with enigmas

--- controllers/LoggedHomepageController.php
+++ controllers/LoggedHomepageController.php
@@ -3,6 +3,7 @@
 namespace ceresia_adventure\controllers;
 
 use ceresia_adventure\framework\LoggedController;
+use ceresia_adventure\models\Trail;
 use ceresia_adventure\repositories\EnigmaRepository;
 use ceresia_adventure\repositories\TrailRepository;
 
@@ -28,8 +29,10 @@ class LoggedHomepageController extends LoggedController
     public function index(): void
     {
         $trailRepository = new TrailRepository();
-        $data_trails = $trailRepository->select(["visible"=> 0])->result_array();
-        echo $this->twig->render('pages/logged_homepage.html.twig', ['data_trails' => $data_trails]);
+        $data_trails = $trailRepository->select(["visible"=> 1])->result_array();
+        $trails = $this->getTrailsWithEnigma($data_trails);
+
+        echo $this->twig->render('pages/logged_homepage.html.twig', ['data_trails' => $trails]);
     }
 
     public function play(): void
