16e616afb8362ff864e454cd1b0c749601c49277
yvanmig <yvan.gimard@gmail.com>
2022-06-13 11:47:30 +0200

    make email unique and verify it before updates

--- database/script_init.sql
+++ database/script_init.sql
@@ -18,10 +18,13 @@ CREATE TABLE users
     `password`     varchar(255) not null,
     `user_type_id` int          not null,
     `departement`  varchar(3)   not null,
-    CONSTRAINT PK_users PRIMARY KEY (`user_id`)
+    CONSTRAINT PK_users PRIMARY KEY (`user_id`),
+    UNIQUE KEY `email` (`email`) USING HASH,
+
 );
 ALTER TABLE users
     ADD CONSTRAINT FK_users_users_types FOREIGN KEY (user_type_id) REFERENCES users_types (user_type_id);
+
 
 CREATE TABLE trails
 (
